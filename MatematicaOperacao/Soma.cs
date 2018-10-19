e3b82b39e9a97fc94a185120be5943f5262f5198
Josiel Lopes <lopes1josiel@gmail.com>
2018-10-19 18:31:55 -0300

    Criando o pacote

---
+++ MatematicaOperacao/Soma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MatematicaOperacao
+{
+    public class Soma
+    {
+        public static double opSomar(double x1,double x2)
+        {
+            return x1 + x2;
+        }
+    }
+}
